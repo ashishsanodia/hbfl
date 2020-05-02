e3548ed80386334dcc8cf98decec692de7705a5f
Ashish Sanodia <ashishsanodia@gmail.com>
2020-05-03 01:52:31 +0530

    uploading localpublic files to s3 bucket

--- scripts/module_04/upload-s3-objects.js
+++ scripts/module_04/upload-s3-objects.js
@@ -2,28 +2,39 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({region: 'us-east-1'})
 
 // Declare local variables
 const s3 = new AWS.S3()
-const bucketName = '/* TODO: Make this bucket name match what you created */'
+const bucketName = 'asanodia-bucket-000'
 
 helpers.getPublicFiles()
-.then(files => uploadS3Objects(bucketName, files))
-.then(data => console.log(data))
+    .then(files => uploadS3Objects(bucketName, files))
+    .then(data => console.log(data))
 
-function uploadS3Objects (bucketName, files) {
-  // TODO: Define putObject params object
+function uploadS3Objects(bucketName, files) {
+    // TODO: Define putObject params object
+    const params = {
+        Bucket: bucketName,
+        ACL: 'public-read'
+    }
 
-  const filePromises = files.map((file) => {
-    const newParams = Object.assign({}, params, {
-      // TODO: Add individual file params
+    const filePromises = files.map((file) => {
+        const newParams = Object.assign({}, params, {
+            // TODO: Add individual file params
+            Body: file.contents,
+            Key: file.name,
+            ContentType: helpers.getContentType(file.name)
+        })
+
+        return new Promise((resolve, reject) => {
+            // TODO: Put objects in S3
+            s3.putObject(newParams, (err, data) => {
+                if (err) reject(err)
+                else resolve(data)
+            })
+        })
     })
 
-    return new Promise((resolve, reject) => {
-      // TODO: Put objects in S3
-    })
-  })
-
-  return Promise.all(filePromises)
+    return Promise.all(filePromises)
 }
