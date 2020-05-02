5c78cd630c1f1ad1c223790ffd1305ab7c7aed85
Ashish Sanodia <ashishsanodia@gmail.com>
2020-05-02 23:51:04 +0530

    Creating launch configuration

--- scripts/module_03/create-launch-configuration.js
+++ scripts/module_03/create-launch-configuration.js
@@ -1,20 +1,39 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({region: 'us-east-1'})
 
 // Declare local variables
 // TODO: Create an autoscaling object
+const autoScaling = new AWS.AutoScaling()
 
-const lcName = 'hamsterLC'
-const roleName = 'hamsterLCRole'
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
+const lcName = 'asanodiaLC'
+const roleName = 'asanodiaLCRole'
+const sgName = 'asanodia_sg'
+const keyName = 'asanodia_key'
 
 helpers.createIamRole(roleName)
-.then(profileArn => createLaunchConfiguration(lcName, profileArn))
-.then(data => console.log(data))
+    .then(profileArn => createLaunchConfiguration(lcName, profileArn))
+    .then(data => console.log(data))
 
-function createLaunchConfiguration (lcName, profileArn) {
-  // TODO: Create a launch configuration
+function createLaunchConfiguration(lcName, profileArn) {
+    // TODO: Create a launch configuration
+    const params = {
+        IamInstanceProfile: profileArn,
+        ImageId: 'ami-0be53bbb527c775d6',
+        InstanceType: 't2.micro',
+        LaunchConfigurationName: lcName,
+        KeyName: keyName,
+        SecurityGroups: [
+            sgName
+        ],
+        UserData: ''
+    }
+
+    return new Promise((resolve, reject) => {
+        autoScaling.createLaunchConfiguration(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
