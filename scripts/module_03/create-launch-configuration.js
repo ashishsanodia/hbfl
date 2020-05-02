fade8a046e77a9e89a7632be96eacbcab96f1d04
Ashish Sanodia <ashishsanodia@gmail.com>
2020-05-03 00:16:43 +0530

    Creating launch configuration

--- scripts/module_03/create-launch-configuration.js
+++ scripts/module_03/create-launch-configuration.js
@@ -27,7 +27,7 @@ function createLaunchConfiguration(lcName, profileArn) {
         SecurityGroups: [
             sgName
         ],
-        UserData: ''
+        UserData: 'IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9hc2hpc2hzYW5vZGlhL2hiZmwuZ2l0IC9ob21lL2JpdG5hbWkvaGJmbA0KY2hvd24gLVIgYml0bmFtaTogL2hvbWUvYml0bmFtaS9oYmZsDQpjZCAvaG9tZS9iaXRuYW1pL2hiZmwNCnN1ZG8gbnBtIGkNCnN1ZG8gbnBtIHJ1biBzdGFydA=='
     }
 
     return new Promise((resolve, reject) => {
