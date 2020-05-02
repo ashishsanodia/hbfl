4a80b2b0b01f3097f686d4017a6a242f83a98aa7
Ashish Sanodia <ashishsanodia@gmail.com>
2020-05-03 03:54:26 +0530

    populating dynamodb

--- scripts/module_05/populate-dynamo-table.js
+++ scripts/module_05/populate-dynamo-table.js
@@ -2,21 +2,37 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({region: 'us-east-1'})
 
 // Declare local variables
 // TODO: Declare dynamoDB DocumentClient object
+const client = new AWS.DynamoDB.DocumentClient()
 
 helpers.getHamsterData()
-.then(data => populateTable('hamsters', data))
-.then(() => helpers.getRaceData())
-.then(data => populateTable('races', data))
-.then(data => console.log(data))
+    .then(data => populateTable('asanodia_hamsters', data))
+    .then(() => helpers.getRaceData())
+    .then(data => populateTable('asanodia_races', data))
+    .then(data => console.log(data))
 
-function populateTable (tableName, data) {
-  // TODO: Create params const object
+function populateTable(tableName, data) {
+    // TODO: Create params const object
+    const params = {
+        RequestItems: {
+            [tableName]: data.map(item => {
+                return {
+                    PutRequest: {
+                        Item: item
+                    }
+                }
+            })
+        }
+    }
 
-  return new Promise((resolve, reject) => {
-    // TODO: Call batch write function
-  })
+    return new Promise((resolve, reject) => {
+        // TODO: Call batch write function
+        client.batchWrite(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
