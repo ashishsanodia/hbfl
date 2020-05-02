66b5dd70556185be606d27306d2721ef46e4ffa2
Ashish Sanodia <ashishsanodia@gmail.com>
2020-05-03 01:43:24 +0530

    reusing ebs volumein ec2

--- scripts/module_04/reuse-ebs-volume.js
+++ scripts/module_04/reuse-ebs-volume.js
@@ -1,28 +1,44 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({region: 'us-east-1'})
 
 // Declare local variables
 const ec2 = new AWS.EC2()
-const volumeId = '/* TODO: Add the volume to detach/attach */'
-const instanceId = '/* TODO: Add the instance to attach to */'
+const volumeId = 'vol-0a9e50ae96f066ffb'
+const instanceId = 'i-081b969504aa0da53'
 
 detachVolume(volumeId)
-.then(() => attachVolume(instanceId, volumeId))
+    .then(() => attachVolume(instanceId, volumeId))
 
-function detachVolume (volumeId) {
-  // TODO: Configure detachVolume params
+function detachVolume(volumeId) {
+    // TODO: Configure detachVolume params
+    const params = {
+        VolumeId: volumeId
+    }
 
-  return new Promise((resolve, reject) => {
-    // TODO: Detach the volume
-  })
+    return new Promise((resolve, reject) => {
+        // TODO: Detach the volume
+        ec2.detachVolume(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
 
-function attachVolume (instanceId, volumeId) {
-  // TODO: Configure attachVolume params
+function attachVolume(instanceId, volumeId) {
+    // TODO: Configure attachVolume
+    const params = {
+        InstanceId: instanceId,
+        VolumeId: volumeId,
+        Device: '/dev/sdf'
+    }
 
-  return new Promise((resolve, reject) => {
-    // TODO: Attach the volume
-  })
+    return new Promise((resolve, reject) => {
+        // TODO: Attach the volume
+        ec2.attachVolume(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
