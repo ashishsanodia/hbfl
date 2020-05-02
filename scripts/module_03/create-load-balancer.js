fb62b80ebf3051d6441710003e3aec017b15aa59
Ashish Sanodia <ashishsanodia@gmail.com>
2020-05-03 00:17:21 +0530

    creating load balancer

--- scripts/module_03/create-load-balancer.js
+++ scripts/module_03/create-load-balancer.js
@@ -2,70 +2,87 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({region: 'us-east-1'})
 
 // Declare local variables
 // TODO: Create a new ELBv2 object
-const sgName = 'hamsterELBSG'
-const tgName = 'hamsterTG'
-const elbName = 'hamsterELB'
-const vpcId = '/* TODO: Add your VPC Id */'
+const elbv2 = new AWS.ELBv2()
+const sgName = 'asanodiaELBSG'
+const tgName = 'asanodiaTG'
+const elbName = 'asanodiaELB'
+const vpcId = 'vpc-78faf702'
 const subnets = [
-  /* TODO: Add two subnets */
+    /* TODO: Add two subnets */
+    'subnet-348f726b',
+    'subnet-2409f842'
 ]
 
 helpers.createSecurityGroup(sgName, 80)
-.then((sgId) =>
-  Promise.all([
-    createTargetGroup(tgName),
-    createLoadBalancer(elbName, sgId)
-  ])
-)
-.then((results) => {
-  const tgArn = results[0].TargetGroups[0].TargetGroupArn
-  const lbArn = results[1].LoadBalancers[0].LoadBalancerArn
-  console.log('Target Group Name ARN:', tgArn)
-  return createListener(tgArn, lbArn)
-})
-.then((data) => console.log(data))
+    .then((sgId) =>
+        Promise.all([
+            createTargetGroup(tgName),
+            createLoadBalancer(elbName, sgId)
+        ])
+    )
+    .then((results) => {
+        const tgArn = results[0].TargetGroups[0].TargetGroupArn
+        const lbArn = results[1].LoadBalancers[0].LoadBalancerArn
+        console.log('Target Group Name ARN:', tgArn)
+        return createListener(tgArn, lbArn)
+    })
+    .then((data) => console.log(data))
 
-function createLoadBalancer (lbName, sgId) {
-  // TODO: Create a load balancer
+function createLoadBalancer(lbName, sgId) {
+    // TODO: Create a load balancer
+    const params = {
+        Name: lbName,
+        Subnets: subnets,
+        SecurityGroups: [
+            sgId
+        ]
+    }
+
+    return new Promise((resolve, reject) => {
+        elbv2.createLoadBalancer(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
 
-function createTargetGroup (tgName) {
-  const params = {
-    Name: tgName,
-    Port: 3000,
-    Protocol: 'HTTP',
-    VpcId: vpcId
-  }
+function createTargetGroup(tgName) {
+    const params = {
+        Name: tgName,
+        Port: 3000,
+        Protocol: 'HTTP',
+        VpcId: vpcId
+    }
 
-  return new Promise((resolve, reject) => {
-    elbv2.createTargetGroup(params, (err, data) => {
-      if (err) reject(err)
-      else resolve(data)
+    return new Promise((resolve, reject) => {
+        elbv2.createTargetGroup(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
     })
-  })
 }
 
-function createListener (tgArn, lbArn) {
-  const params = {
-    DefaultActions: [
-      {
-        TargetGroupArn: tgArn,
-        Type: 'forward'
-      }
-    ],
-    LoadBalancerArn: lbArn,
-    Port: 80,
-    Protocol: 'HTTP'
-  }
+function createListener(tgArn, lbArn) {
+    const params = {
+        DefaultActions: [
+            {
+                TargetGroupArn: tgArn,
+                Type: 'forward'
+            }
+        ],
+        LoadBalancerArn: lbArn,
+        Port: 80,
+        Protocol: 'HTTP'
+    }
 
-  return new Promise((resolve, reject) => {
-    elbv2.createListener(params, (err, data) => {
-      if (err) reject(err)
-      else resolve(data)
+    return new Promise((resolve, reject) => {
+        elbv2.createListener(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
     })
-  })
 }
